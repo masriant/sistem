739b6e4d9b8bb34de5dfb3415483a68abdffb5b9
masriant <info.masrianto@gmail.com>
2020-08-25 06:06:57 +0700

    Pages #1

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,28 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
+		$this->cachePage(38);
 		return view('welcome_message');
 	}
 
+	//--------------------------------------------------------------------
+
+	// public function commands()
+	// {
+	// 	// command( command: 'cache:clear');
+	// 	command( command: 'migrate:create create_user_table');
+	// 	die('cache has been cleared.');
+	// }
+
+	function showme($page = 'home')
+	{
+		echo 'This page is: ' . $page;
+	}
 	//--------------------------------------------------------------------
 
 }
---
+++ app/Controllers/Pages.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+  public function index()
+  {
+    $this->cachePage(38);
+    return view('welcome_message');
+  }
+
+  //--------------------------------------------------------------------
+
+  function showme($page = 'home')
+  {
+    echo 'This page is: ' . $page;
+  }
+  //--------------------------------------------------------------------
+
+}
